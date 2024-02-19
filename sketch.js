540c14d670239e1be435b2f4b524981329b82d70
Taylor Scott <tayzscott@gmail.com>
2024-02-19 16:07:50 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,42 @@
-// SCENES DON'T WORK
-// https://mveteanu.github.io/p5.SceneManager/
-// https://p5play.org/learn/sprite.html?page=10
-// https://www.codecademy.com/learn/learn-p5js/modules/p5js-interaction/cheatsheet
+
+/*
+ * screens = [title=0, study=1, study1=2, study2=3, study3=4,
+ *                   play=6, song1=7, etc.]
+*/
+let screen = 0;
 
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
+  createCanvas(500, 500);
+
+  // go to title screen
+  let title_button = createButton("screen change test");
+  title_button.position(150, 150);
+  title_button.mousePressed(() => {
+    title_button.remove();
+    changeScene(1);
+  });
+}
+
+function draw() {
+  if (screen == 0) {        // blank screen
     background(220);
-  
-    let button = createButton("click me!");
-    button.position(150, 150);
-    button.mousePressed(() => {
-      // clear();
-      titleScreen();
-      console.log("pressed!");
-    });
   }
-  
-  function titleScreen() {
-    clear();
-    background("blue");
-    text("title screen", 50, 50);
+  else if (screen == 1) {  // switch to title screen
+    background(150);
+    textAlign(CENTER);
+    text("title screen!", 50, 50);
   }
-  
+}
+
+function changeScene(x) {
+  switch (x) {
+    case 0:
+      screen = 0;
+      break;
+    case 1:
+      screen = 1;
+      break;
+    default:
+      break;
+  }
+}
